41f656b7dd99b035d2d31a7382213e147f5ecc56
kimyj <yongjjun8718@gmail.com>
2025-08-02 01:31:35 +0900

    init

---
+++ src/main/java/com/kyj/fmk/AnalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.kyj.fmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzeApplication.class, args);
+	}
+
+}
